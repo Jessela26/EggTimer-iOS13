8c37aa7cdab413fb3a985bbd02222285e5c3d474
Jessela26 <jelbaniqued26@gmail.com>
2023-03-06 22:53:10 -0500

    Added progress bar

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    let eggTimes : [String: Int] = ["Soft": 3, "Medium": 4, "Hard": 7]
+    var secondsRemaining = 60
+    
+    var timer = Timer()
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle!
+        let boilTime = eggTimes[hardness]!
+        
+        print("To achieve \(hardness) boiled egg, you must boil it for \(boilTime) seconds.")
+        
+        secondsRemaining = eggTimes[hardness]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self,                                          selector: #selector(oneSecond(sender:)), userInfo: nil, repeats: true)
+    }
+    
+    @IBOutlet weak var eggPrompt: UILabel!
+    @objc func oneSecond(sender: Timer) {
+        if secondsRemaining > 0{
+            print("\(secondsRemaining) seconds left.")
+            secondsRemaining -= 1
+        }
+        else {
+            print("Timer ended!")
+            eggPrompt.text = "DONE!"
+            timer.invalidate()
+        }
+        
+    }
 }
